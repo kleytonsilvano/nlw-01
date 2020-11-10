d106377959be7f53e4ab9055ace81bed0a3a40ed
kleytonsilvano <kleyton.silvano@gmail.com>
2020-11-10 11:42:10 -0300

    imagens dos items

--- server/readme.md
+++ server/readme.md
@@ -1,3 +1,8 @@
 npm install multer -- Instalar upload de imagem
 npm install @types/multer -D
 npm install celebrate
+
+npm install -- Adicionar todas dependencias no node_modules
+npx knex migrate:latest --knexfile knexfile.ts migrate:latest     --Adicionar as migrations
+npm run knex:seed --Adicionar os items no bd
+npm run dev
--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -2,11 +2,11 @@ import Knex from 'knex';
 
 export async function seed(knex : Knex) {
     await knex('items').insert([
-        { title: 'Lâmpadas', image: 'lampadas.svg'  },
-        { title: 'Pilhas e Baterias', image: 'baterias.svg'  },
-        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg'  },
-        { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'  },
-        { title: 'Resíduos Orgânicos', image: 'organicos.svg'  },
-        { title: 'Óleo de Cozinha', image: 'oleo.svg'  }
+        { title: 'Lâmpadas', image: 'items/lampadas.svg'  },
+        { title: 'Pilhas e Baterias', image: 'items/baterias.svg'  },
+        { title: 'Papéis e Papelão', image: 'items/papeis-papelao.svg'  },
+        { title: 'Resíduos Eletrônicos', image: 'items/eletronicos.svg'  },
+        { title: 'Resíduos Orgânicos', image: 'items/organicos.svg'  },
+        { title: 'Óleo de Cozinha', image: 'items/oleo.svg'  }
     ]);
 }
